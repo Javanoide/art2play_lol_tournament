0bb61f9509a83ff6ccda742a1ed0521636efca80
Florent Baudon <florent.baudon@gmail.com>
2015-10-14 16:11:34 +0200

    ajout list player et team + realtime

--- Server/model.js
+++ Server/model.js
@@ -1,5 +1,5 @@
 var redis = require('redis');
-var client = redis.createClient(8080, '78.193.226.46', {});
+var client = redis.createClient(6379, '127.0.0.1', {});
 
 module.exports.subscribe = function(username, team, callback){
   //inscription du joueur de une HM, contient nom de la team et autre si besoin (email, date de naissance, etc...)
@@ -8,15 +8,38 @@ module.exports.subscribe = function(username, team, callback){
   client.zadd('team:' + team, Date.now(), username);
   //Ajout de la team à la liste des team
   client.zadd('teamlist', Date.now(), team);
+  //Ajout du joueur à la liste des joueurs
+  client.zadd('playerlist', Date.now(), username);
+
   callback({success : true, username : username, team : team});
 };
 
 module.exports.getUser = function(username, callback){
-
+  clietn.hgetall('user:' + username, function (err, response){
+    if(!err){
+      callback({success : true, response : response});
+    }
+  });
 };
 
 module.exports.getTeam = function(team, callback){
   client.zrange('team:' + team, 0, -1, function (err, response){
+    if(!err){
+      callback({success : true, response : response});
+    }
+  });
+};
+
+module.exports.getTeamList = function(callback){
+  client.zrange('teamlist', 0, -1, function (err, response){
+    if(!err){
+      callback({success : true, response : response});
+    }
+  });
+};
+
+module.exports.getPlayerList= function(callback){
+  client.zrange('playerlist', 0, -1, function (err, response){
     if(!err){
       callback({success : true, response : response});
     }
--- Server/socket.js
+++ Server/socket.js
@@ -3,24 +3,41 @@ module.exports = function(server){
   var io = require('socket.io')(server);
 
   io.sockets.on('connection', function(socket){
+    //envoie la liste des joueurs
+    model.getPlayerList(function(result){
+      socket.emit('getplayerlist', result.response);
+    });
+    //envoie la liste de équipes
+    model.getTeamList(function(result){
+      console.log(result.response);
+      socket.emit('getteamlist', result.response);
+    });
+
     //Inscription d'un joeur
     socket.on('subscribe', function(username, team){
-
       model.subscribe(username, team, function (result){
         console.log('Subscribe : ' + result.username + ' - ' + result.team + ' : ' + result.success);
+        //mise à jour des autres clients
+        model.getTeamList(function(result){
+          console.log(result.response);
+          io.emit('getteamlist', result.response);
+        });
+        model.getPlayerList(function(result){
+          io.emit('getplayerlist', result.response);
+        });
+
       });
     });
     //recupération des membres d'une équipe
     socket.on('getteam', function(team){
       model.getTeam(team, function(result){
-        console.log(result.response);
-        io.emit('getteam', result.response);
+        socket.emit('getteam', result.response);
       });
     });
     //récupération des détails d'un joueur
-    socket.on('getuser', function(team){
+    socket.on('getuser', function(username){
       model.getUser(username, function(result){
-
+        socket.emit('getuser', result.response);
       });
     });
 
